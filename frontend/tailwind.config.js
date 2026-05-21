d5e60b9dea5580c354679215a2242cf98077ffe7
MARCOS <marcosdsalazar39@gmail.com>
2026-05-21 16:06:29 -0300

    cambie cosas del css

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,13 +8,6 @@ export default {
     extend: {
       fontFamily: {
         playfair: ['Playfair Display', 'serif'],
-        pacifico: ['Pacifico', 'cursive'],
-        lobster: ['Lobster', 'sans-serif'],
-        cinzel: ['Cinzel Decorative', 'serif'],
-        paris: ['Parisienne', 'cursive'],
-        orbitron: ['Orbitron', 'sans-serif'],
-        creep: ['Creepster', 'system-ui'],
-        montserrat: ['Montserrat', 'sans-serif'],
       },
     },
   },
