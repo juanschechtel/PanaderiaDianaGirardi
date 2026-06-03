34a5532c5ce4fa821c3a31cfaa5a65fe5d1572bb
Marcosd145 <marcosdsalazar39@gmail.com>
2026-06-03 18:31:50 -0300

    react agregado

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Productos from './pages/Productos';
+import Reservas from './pages/Reservas';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/productos" element={<Productos />} />
+        <Route path="/reservas" element={<Reservas />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
