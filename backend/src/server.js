cd943982e11d793678bc81f9c262da5b117b8d97
WillimanC <cristianwilliman@gmail.com>
2026-06-03 17:38:26 -0300

    Montamos el servidor backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,17 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
 
-dotenv.config();
+import express from 'express';
 
 const app = express();
 
-app.use(express.json());
-app.use(cors());
+const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Backend Funcionando");
-});
+  res.send("Marcos wachin");
+}
+)
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server is running on port http://localhost:${process.env.PORT || 3000}`);
-});
+
+app.listen(port, () => {
+  console.log("Server corriendo");
+})
 
