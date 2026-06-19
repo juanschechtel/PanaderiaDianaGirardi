9ca40b5401c7768b6f218f40aa4aeb335da147ca
Marcosd145 <marcosdsalazar39@gmail.com>
2026-06-19 18:50:59 -0300

    Se puso un fondo para el apartado de home (Pz,Ms-20Minutos)

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center">
+    <main className="bg-[url('https://media.scoolinary.app/blog/images/2023/11/vista-de-croissant-gourmet-recien-horneados.jpg')] bg-cover flex flex-col items-center justify-center">
       <h1>Panadería Diana Girardi</h1>
       <p>Bienvenidos. Pan artesanal hecho con dedicación.</p>
     </main>
