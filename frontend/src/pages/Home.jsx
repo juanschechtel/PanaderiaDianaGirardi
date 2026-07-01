a697791b28393e1b094677c95fb75a973733a27e
zunpaula <zunpaula@gmail.com>
2026-07-01 18:55:48 -0300

    Agregamos imagenes a la página de inicio (ms y pz)

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main id="inicio" className="bg-[url('https://media.scoolinary.app/blog/images/2023/11/vista-de-croissant-gourmet-recien-horneados.jpg')] bg-cover backdrop-blur flex flex-col items-center justify-center w-screen h-screen">
-      <div className=" text-black ">
+      <div className=" text-white ">
       <h1>Panadería Diana Girardi</h1>
       <p>Bienvenidos. Pan artesanal hecho con dedicación.</p>
       </div>
