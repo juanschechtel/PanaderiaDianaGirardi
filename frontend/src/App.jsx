72edfa559a870016bda6cd25bc3856db1656541f
WillimanC <cristianwilliman@gmail.com>
2026-07-08 17:30:11 -0300

    Corregi una mayuscula

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,17 @@ import Home from './pages/Home'
 import Productos from './pages/Productos'
 import Nosotros from './pages/Nosotros'
 import Reservas from './pages/Reservas'
-import Nav from './components/Nav'
+import Nav from './components/nav'
 import './App.css'
 
 export default function App() {
   return (
     <>
-      <Nav/>
+      <Nav />
       <Home />
-      <Productos/>
-      <Nosotros/>
-      <Reservas/>
+      <Productos />
+      <Nosotros />
+      <Reservas />
     </>
   )
 }
